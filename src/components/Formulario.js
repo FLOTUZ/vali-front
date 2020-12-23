8e3aa3857057b8aac5af88147d12d3942fd57e43
Flotuz <flotuz10@gmail.com>
2020-12-23 00:38:51 -0600

    En el formulario se dió toda la funcionalidad del login

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,38 +1,92 @@
-import { useState } from 'react';
+import NavBar from "./NavBar";
+import { useState } from "react";
+const axios = require("axios").default;
+
 export default function Formulario(props) {
-    //hook
+  //hook
 
-    const [usuario, setUsuario] = useState('');
-    const [password, setPassword] = useState('');
+  const [user, setUser] = useState("");
+  const [pass, setPass] = useState("");
+  const [indicador, setIndi] = useState("");
 
-    return (
-        <div>
-            {nav()}
-            <form action="http://localhost:4000/login" method="GET">
-                <div className="mb-3">
-                    <label htmlFor="user" className="form-label">Usuario</label>
-                    <input type="text" className="form-control" id="user" aria-describedby="emailHelp" name="user" value={usuario} onChange={(e) => setUsuario(e.target.value)}></input>
+  function validar(evento) {
+    //Previene el comportamiento por defecto de un submit
+    evento.preventDefault();
 
-                    <div className="mb-3">
-                        <label htmlFor="passwordField" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="passwordField" name="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                    </div>
-
-                    <button type="submit" className="btn btn-outline-dark btn-lg">Dale</button>
-                </div>
-            </form>
-        </div>
-    );
-}
-
-function nav() {
-    return (
-        <nav className="navbar navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">
-                    Login
-                </a>
+    /*
+            La validación se hace en el backend, 
+            si lanza un correo en el then, significa que el usuario existe
+            de caso contrario, manda un mensaje de error
+    
+            El catch solo está por si ocurre un error muy raro
+            */
+    axios
+      .get("http://localhost:4000/login", {
+        params: {
+          correo: user,
+          password: pass,
+        },
+      })
+      .then((response) => {
+        if (response.data.access) {
+          props.history.push("/home");
+        } else {
+          let indicadorLogin = (
+            <div className="form-control is-invalid">
+              {response.data.message}
             </div>
-        </nav>
-    );
+          );
+          setIndi(indicadorLogin);
+        }
+        //Esta wea deberia de redireccionar
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  return (
+    <div>
+      <NavBar tittle="Login" />
+      <form onSubmit={(e) => validar(e)}>
+        <div className="mb-3">
+          {indicador}
+          <br />
+          <label htmlFor="user" className="form-label">
+            Correo
+          </label>
+
+          <input
+            type="text"
+            className="form-control"
+            id="user"
+            onClick={() => setIndi("")}
+            aria-describedby="emailHelp"
+            name="user"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            placeholder="Correo"
+          ></input>
+          <br />
+          <label htmlFor="passwordField" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="passwordField"
+            onClick={() => setIndi("")}
+            name="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+            placeholder="password"
+          ></input>
+          <br />
+          <button type="submit" className="btn btn-outline-dark btn-lg">
+            Dale
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
