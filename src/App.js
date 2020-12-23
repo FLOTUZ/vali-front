0e199f7fab608ced311a8f3f997e6888436451ca
Flotuz <flotuz10@gmail.com>
2020-12-23 00:40:37 -0600

    Se agregó la ruta home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
-import React, {Suspense, lazy} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const formulario = lazy(() => import('./components/Formulario'));
+const formulario = lazy(() => import("./components/Formulario"));
+const home = lazy(() => import("./components/Home"));
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Suspense fallback={<div> loading...</div>}>
         <Switch>
           <Route exact path="/" component={formulario}></Route>
+          <Route exact path="/home" component={home}></Route>
         </Switch>
       </Suspense>
     </Router>
