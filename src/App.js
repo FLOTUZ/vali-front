39dc122f0d2961e49e0d1cf5adff2067cf2acd70
Flotuz <flotuz10@gmail.com>
2020-12-29 18:36:36 -0600

    Se agregó la librera de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
